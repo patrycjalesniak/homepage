aff7c7b66c0d7a76ffa9ab7a443135929fef841f
Patrycja Leśniak <patrycjalesniak94@gmail.com>
2020-05-10 17:35:51 +0200

    second commit

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-alert('Witaj na mojej stronce!');
+const firstName = "Patrycja";
 
-const firstName='Patrycja';
+console.log(
+    `Witaj Drogi Odwiedzający, nazywam się ${firstName}`);
 
-console.log(`Witaj Drogi Odwiedzający, nazywam się ${firstName}`)
+const heading = document.querySelector('.hotpink-header--js');
+
+console.log(heading.innerHTML);
+
+heading.innerHTML = `Witaj Drogi Odwiedzający, nazywam się ${firstName}`
+
