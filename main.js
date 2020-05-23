76f79f1529744522f37ac8ccd04be80b8fa24008
Patrycja Leśniak <patrycjalesniak94@gmail.com>
2020-05-23 21:05:08 +0200

    five weeks finised

--- main.js
+++ main.js
@@ -1,15 +1,86 @@
 const firstName = "Patrycja";
+const age = "26";
 
-console.log(
-    `Witaj Drogi Odwiedzający, nazywam się ${firstName}`);
+console.log(`Witaj Drogi Odwiedzający, nazywam się ${firstName}`);
 
-const heading = document.querySelector('.hotpink-header--js');
+const heading = document.querySelector(".hotpink-header--js");
 
 console.log(heading.innerHTML);
 
 heading.innerHTML = `Witaj Drogi Odwiedzający, nazywam się ${firstName}`;
 
-const emptyParagraph = document.querySelector('.week-summary__description--js');
+const greet=(age,firstName) =>{
+    console.log(
+        `Hej! Miło, że tutaj zaglądasz, nazywam się ${firstName} i mam ${age} lata.`
+    );
+}
 
+greet(26,'Pati');
+
+const emptyParagraph = document.querySelector(".week-summary__description--js");
 
 emptyParagraph.innerHTML = `Dobre pytanie!`;
+
+function calculate(myNumber) {
+  console.log(`Dostałam ${myNumber}`);
+  myNumber = myNumber * 7;
+  return `wynik ${myNumber}`;
+}
+
+const myResult = calculate(age);
+console.log(age);
+console.log(myResult);
+
+let myNumber = "3";
+const timesFive = (myNumber) => {
+  console.log(`Podana liczba to ${myNumber}`);
+  return myNumber * 5;
+};
+let timesFiveResult = timesFive(myNumber);
+console.log(
+  `Wynik mnożenia liczby ${myNumber} razy 5 wynosi ${timesFiveResult}`
+);
+
+myNumber = "5";
+timesFiveResult = timesFive(myNumber);
+console.log(
+  `Wynik mnożenia liczby ${myNumber} razy 5 wynosi ${timesFiveResult}`
+);
+
+
+
+const weekNumber = "5";
+
+function createContent(querySelectorContent, content) {
+  const element = document.querySelector(querySelectorContent);
+  element.innerHTML = content;
+}
+    <!--teks do nagłówka dodany za pomocą JS-->
+
+createContent(
+  ".secondary-heading__welcome--js",
+  `HEJ! JESTEM PATRYCJA I... `
+);
+
+console.log("Więcej funkcji liczących coś prostego:");
+
+const timesFour = (someNumber) => {
+  inputNumber = someNumber;
+  console.log(`podana liczba to ${inputNumber}`);
+  timesFourResult = someNumber * 4;
+  console.log(
+    `wynik mnożenia liczby ${inputNumber} razy 4 to: ${timesFourResult}`
+  );
+};
+
+timesFour(5);
+
+function calculateMultiplication(firstInputNumber, secondInputNumber) {
+  console.log(`podane liczby to ${firstInputNumber} i ${secondInputNumber}`);
+  multiplicationResult = firstInputNumber * secondInputNumber;
+  console.log(`wynik mnożenia to ${multiplicationResult}`);
+}
+
+calculateMultiplication(6, 8);
+calculateMultiplication(2, 2);
+calculateMultiplication(12, 3);
